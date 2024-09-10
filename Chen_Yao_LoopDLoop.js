3c7a70d8ad29fd614c92b38777a7cc0513ee44ba
eddiechan <eddie.chan0530@gmail.com>
2024-09-09 23:00:53 -0700

    Add Chen_Yao_LoopDLoop.js

---
+++ Chen_Yao_LoopDLoop.js
@@ -0,0 +1,13 @@
+var distance;
+var speed;
+
+for (distance=2; distance<6; distance += 2)
+    {
+        console.log("Pop out a piece of candy");
+    }
+
+for (distance=2; distance >0 && speed >=5.5; distance += 2)
+    {
+        console.log("Pop out a piece of candy");
+    }
+
